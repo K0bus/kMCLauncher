6ac9cee97a8277dc330b58bd4fa93693d9756213
K0bus <k0bus@mail.com>
2020-01-22 19:06:28 +0100

    Adding more test

--- src/test/java/kmclauncher/AppTest.java
+++ src/test/java/kmclauncher/AppTest.java
@@ -4,35 +4,44 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import kmclauncher.auth.MinecraftUser;
+import kmclauncher.launch.LaunchType;
+import kmclauncher.launch.Launcher;
+import kmclauncher.update.Updater;
+import kmclauncher.utils.Configurator;
+import kmclauncher.utils.Logger;
+
 public final class AppTest {
     private AppTest() {
     }
 
+    private static File MC_GAMEDIR = new File(System.getenv("APPDATA") + File.separator + ".akuragaming");
+    private static String MC_VERSION = "1.12.2";
+    private static String DL_URL = "http://127.0.0.1/kMCUpdate-Server/";
+
     public static void main(String[] args) {
         try {
-            Updater updater = new Updater(new URL("http://127.0.0.1/kMCUpdate-Server/"),
-                    new File(System.getenv("APPDATA") + File.separator + ".akuragaming"), "1.12.2");
-            if(updater.removeBadFile("mods")){
+            Updater updater = new Updater(new URL(DL_URL), MC_GAMEDIR, MC_VERSION);
+            if (updater.removeBadFile("mods")) {
                 Logger.info("Remover successfully runned");
-            }
-            else
-            {
+            } else {
                 Logger.error("Error with Remover");
             }
-            if(updater.updateMojang()){
+            if (updater.updateMojang()) {
                 Logger.info("Mojang updater was successfully done");
-            }
-            else
-            {
+            } else {
                 Logger.error("Error with Mojang Updater");
             }
-            if(updater.updateServer()){
+            if (updater.updateServer()) {
                 Logger.info("Modpack update done");
-            }
-            else
-            {
+            } else {
                 Logger.error("Error with Modpack update");
             }
+            Configurator config = new Configurator(MC_GAMEDIR, "launcher_options.json");
+            MinecraftUser user = config.getUser();
+            user.isAuth();
+            Launcher launcher = new Launcher(LaunchType.Vanilla, MC_GAMEDIR, "1.12.2", user);
+            launcher.run();
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
